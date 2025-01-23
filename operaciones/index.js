49cfbffaa96e326f20f5c712aed3eb1d28d0b91f
aleeextl <alextriguerosleon@gmail.com>
2025-01-23 19:02:57 +0100

    funcions aplicades

--- operaciones/index.js
+++ operaciones/index.js
@@ -5,7 +5,11 @@
  * @returns {Number} el resultat de la operacio
  */
 function suma(...valors) {
-
+    let resultat = 0
+    for (let i = 0; i < valors.length; i++) {
+        resultat += valors[i];
+    }
+    return resultat;
 }
 
 
@@ -16,7 +20,11 @@ function suma(...valors) {
  * @returns {Number} el resultat de la operacio
  */
 function multiplica(...valors) {
-
+    let resultat = 1
+    for (let i = 0; i < valors.length; i++) {
+        resultat *= valors[i];
+    }
+    return resultat;
 }
 
 module.exports.suma = suma;
